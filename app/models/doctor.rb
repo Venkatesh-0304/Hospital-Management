3440439298a5bd187f7a99c0477c7cac07591730
Venkatesh-0304 <156488647+Venkatesh-0304@users.noreply.github.com>
2026-06-23 13:03:46 +0530

    Join tables created

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,13 @@
 class Doctor < ApplicationRecord
-  has_and_belongs_to_many :hospital
+  has_and_belongs_to_many :hospitals
   has_one :doctor_profile
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :patients, through: :appointments
+
+  before_destroy :log_destroy
+
+  private
+  def log_destroy
+    puts "Printing Before destory"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_22_064236) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_22_065854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_22_064236) do
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "medicines_patients", id: false, force: :cascade do |t|
+    t.bigint "medicine_id", null: false
+    t.bigint "patient_id", null: false
   end
 
   create_table "patients", force: :cascade do |t|
